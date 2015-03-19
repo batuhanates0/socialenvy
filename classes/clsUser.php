a82e859cd3502dd61a65f5751c54889853b10297
pankajmishra27 <pankaj@resiliencesoft.com>
2015-03-20 01:20:06 +0530

    new changes

--- classes/clsUser.php
+++ classes/clsUser.php
@@ -9,32 +9,85 @@ include "DatabaseConnection.php";
 
 class clsUser
 {
-
-//    private $dbHost = "localhost";
-//    private $dbName = "socialen_instamgt";
-//    private $dbUser = "socialen";
-//    private $dbPassword = "y2QnRqtW";
-
-    public function Login($Username, $Password)
+    public function Login($email, $password)
     {
-        return "Hello";
+        $dbCon = new DatabaseConnection();
+
+        $result = $dbCon->con->prepare("SELECT * FROM tblLogin where email=:email and password=::password");
+
+        $result->bindParam(':email', $email);
+        $result->bindParam(':password', $password);
+
+        $result->execute();
+
+        $userCount = $result->rowCount();
+
+        if ($userCount == 1) {
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        return $userCount;
     }
 
     public function GetUsers()
     {
-        echo "Start Connecting";
-        //$this->$con = new PDO("mysql:host=$this->dbHost;dbname=$this->dbName", $this->dbUser, $this->dbPassword);
+        $dbCon = new DatabaseConnection();
 
-       $dbCon = new DatabaseConnection();
-//        $dbCon->Connect();
-
-        echo "connected";
-        $dbCon->con->
-        $result = $dbCon->$dbCon->prepare("SELECT * FROM tblLogin");
+        $result = $dbCon->con->prepare("SELECT * FROM tblLogin");
         $result->execute();
 
-        $userCount = $getLogins->rowCount();
-        echo "count row";
+        $userCount = $result->rowCount();
         return $userCount;
+    }
+
+    public function GetUser($email)
+    {
+        $dbCon = new DatabaseConnection();
+
+        $result = $dbCon->con->prepare("SELECT * FROM tblLogin where email=:email");
+        $result->bindParam(':email', $email);
+        $result->execute();
+
+        $userCount = $result->rowCount();
+        return $userCount;
+    }
+
+    public function Register($email, $password, $status)
+    {
+        $dbCon = new DatabaseConnection();
+        $result = $dbCon->con->prepare("SELECT * FROM tblLogin where email=:email");
+        $result->bindParam(':email', $email);
+
+        $result->execute();
+        $userCount = $result->rowCount();
+
+        if ($userCount == 0) {
+
+            $date = date('Y-m-d');
+
+            $strQuery = "insert into tblLogin (`email`,`password`,`status`,`date`)
+                              VALUES (:email,:password,:status,:date)";
+
+            $register_result = $dbCon->con->prepare($strQuery);
+
+            $register_result->bindParam(':email', $email);
+            $register_result->bindParam(':password', $password);
+            $register_result->bindParam(':status', $status);
+            $register_result->bindParam(':date', $date);
+
+            $register_result->execute();
+            $rows = $register_result->rowCount();
+
+            if ($rows == 1) {
+                $_SESSION['login_user'] = $email; // Initializing Session
+                echo "success";
+            } else {
+                $responseMessage = "failed";
+            }
+        } else {
+            echo "Email already Exists";
+        }
+
     }
 }
--- clstest.php
+++ clstest.php
@@ -1,36 +1,11 @@
 <?php
-/**
-* Created by PhpStorm.
- * User: DIGICOM
-* Date: 03/19/2015
-* Time: 5:05 PM
-*/
-
-//$dbHost = "localhost";
-//$dbName = "socialen_instamgt";
-//$dbUser = "socialen";
-//$dbPassword = "y2QnRqtW";
-//
-//$con = new PDO("mysql:host=$dbHost;dbname=$dbName",$dbUser,$dbPassword);
-//
-//$check_email=$con->prepare("SELECT * FROM tblLogin");
-//
-//$check_email->execute();
-//$no_email=$check_email->rowCount();
-//echo $no_email;
 
 include("classes/clsUser.php");
-
-
-
 
 $user = new clsUser();
 
 //$response = $user->Login('pankaj','mishra');
 //echo $response;
 
-echo 'Current PHP version: ' . phpversion();
-echo "start user";
 $count = $user->GetUsers();
 echo $count;
-echo "end user";
