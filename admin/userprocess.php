4256677d3c82e1d05396063a2535c3dc37658c89
NirdeshJain <nirdesh@resiliencesoft.com>
2015-04-09 16:11:27 +0530

    Commit userprocess page

--- admin/userprocess.php
+++ admin/userprocess.php
@@ -24,7 +24,7 @@
     <div class="row-fluid sortable">
         <div class="box span12">
             <div class="box-header well" data-original-title>
-                <h2><i class="icon-user"></i>Performed Actions Of Users</h2>
+                <h2><i class="icon-user"></i>Actions Performed By Users</h2>
                 <div class="box-icon">
                     <!--        <a href="#" class="btn btn-setting btn-round"><i class="icon-cog"></i></a>-->
                     <!--        <a href="#" class="btn btn-minimize btn-round"><i class="icon-chevron-up"></i></a>-->
