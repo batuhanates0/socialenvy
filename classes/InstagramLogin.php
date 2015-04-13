77c024972c923c0f37c42f84f1576df0d7d6ddf1
NirdeshJain <nirdesh@resiliencesoft.com>
2015-04-13 16:18:23 +0530

    commit by Nirdesh

--- classes/InstagramLogin.php
+++ classes/InstagramLogin.php
@@ -76,6 +76,7 @@ class InstagramLogin
                 $isRunning='Stopped';
 
 
+
                 $strQuery = "insert into tblInstagram (`uid`,`unique_username`,`IGuname`,`IGpassword`,`IGAccstatus`,`date`,`isRunning`)
                               VALUES (:uid,:unique_username,:IGuname,:IGpassword,:IGAccstatus,:date,:isRunning)";
 
@@ -93,9 +94,110 @@ class InstagramLogin
 
                 $result->execute();
 
+                $rows = $result->rowCount();
+
+                //instagram table id fetch code
+                $fetch_InsAccid=$dbCon->con->prepare("SELECT id FROM tblInstagram WHERE IGuname = :IG_uname");
+                $fetch_InsAccid->bindparam(':IG_uname',$username);
+                $fetch_InsAccid->execute();
+                $fetch_InsAccid = $fetch_InsAccid->fetch(PDO::FETCH_ASSOC);
+
+                $InsAccid=$fetch_InsAccid['id'];
+
+
+                //instagram table fetch id code end
+
+
+                /////////Main setting table insert code
+                $Activityspeed="slow";
+                $Mediasource="Tags";
+                $Minlikesfilter=0;
+                $Maxlikesfilter=0;
+                $Newmediaonly="false";
+                $Dontcommentsameusers="false";
+
+                $strQuery = "insert into tblMainSetting (`uid`,`InstaAccId`,`Activityspeed`,`Mediasource`,`Minlikesfilter`,`Maxlikesfilter`,`Newmediaonly`,`Dontcommentsameusers`)
+                              VALUES (:uid,:InsAccid,:Activityspeed,:Mediasource,:Minlikesfilter,:Maxlikesfilter,:Newmediaonly,:Dontcommentsameusers)";
+                $result_mainsetting = $dbCon->con->prepare($strQuery);
+
+                $result_mainsetting->bindParam(':uid', $uid);
+                $result_mainsetting->bindParam(':InsAccid', $InsAccid);
+                $result_mainsetting->bindParam(':Activityspeed', $Activityspeed);
+                $result_mainsetting->bindParam(':Mediasource', $Mediasource);
+                $result_mainsetting->bindParam(':Minlikesfilter', $Minlikesfilter);
+                $result_mainsetting->bindParam(':Maxlikesfilter', $Maxlikesfilter);
+                $result_mainsetting->bindParam(':Newmediaonly', $Newmediaonly);
+                $result_mainsetting->bindParam(':Dontcommentsameusers', $Dontcommentsameusers);
+
+                $result_mainsetting->execute();
+
+                $rows_mainsetting = $result_mainsetting->rowCount();//  fetch(PDO::FETCH_NUM);
+
+                ///////////////////////////main setting table insert code end
+
+                //Auto Stop setting tbl code
+                $Likescounter=0;
+                $Commentscounter=0;
+                $Followscounter=0;
+                $Unfollowscounter=0;
+                $Timer="00:00";
+
+
+                $strQuery1 = "insert into tblAutoStopSetting (`uid`,`InstaAccId`,`Likescounter`,`Commentscounter`,`Followscounter`,`Unfollowscounter`,`Timer`)
+                              VALUES (:uid,:InsAccid,:Likescounter,:Commentscounter,:Followscounter,:Unfollowscounter,:Timer)";
+                $result_autosetting = $dbCon->con->prepare($strQuery1);
+
+                $result_autosetting->bindParam(':uid', $uid);
+                $result_autosetting->bindParam(':InsAccid',$InsAccid);
+                $result_autosetting->bindParam(':Likescounter', $Likescounter);
+                $result_autosetting->bindParam(':Commentscounter', $Commentscounter);
+                $result_autosetting->bindParam(':Followscounter', $Followscounter);
+                $result_autosetting->bindParam(':Unfollowscounter', $Unfollowscounter);
+                $result_autosetting->bindParam(':Timer', $Timer);
+
+                $result_autosetting->execute();
+
+                $rows_autosetting = $result_autosetting->rowCount();//  fetch(PDO::FETCH_NUM);
+                // auto stop seeting code end
+
+                //usertag table insert code
+                $check_hashtag=$dbCon->con->prepare("SELECT hashtagname FROM tblHashTag");
+
+                $check_hashtag->execute();
+               // $result_hashtag = $check_hashtag->fetchall();
+
+
+                while($result = $check_hashtag->fetch(PDO::FETCH_OBJ))
+                {
+                   foreach($result as $val){
+
+
+                       $strQuery1 = "insert into tblUsertag (`tagname`,`uid`,`InstaAccId`)
+                              VALUES (:tag,:uid,:InstaAccId)";
+                       $result_usertag = $dbCon->con->prepare($strQuery1);
+
+                       $result_usertag->bindParam(':tag', $val);
+                       $result_usertag->bindParam(':uid', $uid);
+                       $result_usertag->bindParam(':InstaAccId', $InsAccid);
+
+                       $result_usertag->execute();
+
+                       $rows_usertag= $result_usertag->rowCount();
+
+
+                   }
+                }
+
+
+
+
+
+
+                //end of usertag table insert code
+
                 //$responseMessage = "Error Code : " .$result->errorCode() . '<br><br>';
                 //echo json_encode($responseMessage);
-                $rows = $result->rowCount();
+
 
                 if ($rows == 1) {
                     // $_SESSION['login_user'] = $email; // Initializing Session
